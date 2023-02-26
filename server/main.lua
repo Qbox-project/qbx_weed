59b42f00c0cd0122fe8936c9c6c5ab8c6f7aaccc
HebiKotei <c1yvan@outlook.com>
2023-02-26 19:49:46 +0000

    refactor: Extracting functions, general tidying

--- server/main.lua
+++ server/main.lua
@@ -8,7 +8,7 @@ QBCore.Functions.CreateCallback('qb-weed:server:getBuildingPlants', function(_, 
             buildingPlants[#buildingPlants + 1] = plants[i]
         end
 
-        if buildingPlants ~= nil then
+        if buildingPlants then
             cb(buildingPlants)
         else
             cb(nil)
@@ -18,11 +18,9 @@ end)
 
 RegisterNetEvent('qb-weed:server:placePlant', function(coords, sort, currentHouse)
     local random = math.random(1, 2)
-    local gender
+    local gender = "woman"
     if random == 1 then
         gender = "man"
-    else
-        gender = "woman"
     end
     MySQL.insert('INSERT INTO house_plants (building, coords, gender, sort, plantid) VALUES (?, ?, ?, ?, ?)', { currentHouse, coords, gender, sort, math.random(111111, 999999) })
     TriggerClientEvent('qb-weed:client:refreshHousePlants', -1, currentHouse)
@@ -33,63 +31,71 @@ RegisterNetEvent('qb-weed:server:removeDeathPlant', function(building, plantId)
     TriggerClientEvent('qb-weed:client:refreshHousePlants', -1, building)
 end)
 
-CreateThread(function()
+---@param plants table
+local function checkHousePlantFood(plants)
+    if plants.food >= 50 then
+        MySQL.update('UPDATE house_plants SET food = ? WHERE plantid = ?', { (plants.food - 1), plants.plantid })
+        if plants.health + 1 < 100 then
+            MySQL.update('UPDATE house_plants SET health = ? WHERE plantid = ?', { (plants.health + 1), plants.plantid })
+        end
+    end
+
+    if plants.food < 50 then
+        if plants.food - 1 >= 0 then
+            MySQL.update('UPDATE house_plants SET food = ? WHERE plantid = ?', { (plants.food - 1), plants.plantid })
+        end
+        if plants.health - 1 >= 0 then
+            MySQL.update('UPDATE house_plants SET health = ? WHERE plantid = ?', { (plants.health - 1), plants.plantid })
+        end
+    end
+end
+
+local function manageHousePlants()
     while true do
         local housePlants = MySQL.query.await('SELECT * FROM house_plants', {})
-        for k, _ in pairs(housePlants) do
-            if housePlants[k].food >= 50 then
-                MySQL.update('UPDATE house_plants SET food = ? WHERE plantid = ?', { (housePlants[k].food - 1), housePlants[k].plantid })
-                if housePlants[k].health + 1 < 100 then
-                    MySQL.update('UPDATE house_plants SET health = ? WHERE plantid = ?', { (housePlants[k].health + 1), housePlants[k].plantid })
-                end
-            end
-
-            if housePlants[k].food < 50 then
-                if housePlants[k].food - 1 >= 0 then
-                    MySQL.update('UPDATE house_plants SET food = ? WHERE plantid = ?', { (housePlants[k].food - 1), housePlants[k].plantid })
-                end
-                if housePlants[k].health - 1 >= 0 then
-                    MySQL.update('UPDATE house_plants SET health = ? WHERE plantid = ?', { (housePlants[k].health - 1), housePlants[k].plantid })
-                end
-            end
+        for i in #housePlants do
+            checkHousePlantFood(housePlants[i])
         end
         TriggerClientEvent('qb-weed:client:refreshPlantStats', -1)
         Wait((60 * 1000) * 19.2)
     end
-end)
+end
 
-CreateThread(function()
+---@param plants table
+---@return string nextStage
+local function getNextStage(plant)
+    local initStage = tonumber(string.sub(plant.stage, -1))
+    return "stage-" .. tostring(initStage + 1)
+end
+
+---@param plants table
+local function growPlant(plant)
+    if plant.health <= 50 then return end
+    local grow = math.random(1, 3)
+    if plant.progress + grow < 100 then
+        MySQL.update('UPDATE house_plants SET progress = ? WHERE plantid = ?', { (plant.progress + grow), plant.plantid })
+        return
+    end
+    if plant.stage == QBWeed.Plants[plant.sort]["highestStage"] then return end
+    if plant.stage then
+        MySQL.update('UPDATE house_plants SET stage = ? WHERE plantid = ?', { getNextStage(plant.stage), plant.plantid })
+    end
+    MySQL.update('UPDATE house_plants SET progress = ? WHERE plantid = ?', { 0, plant.plantid })
+end
+
+local function updatePlantGrowth()
     while true do
         local housePlants = MySQL.query.await('SELECT * FROM house_plants', {})
-        for k, _ in pairs(housePlants) do
-            if housePlants[k].health > 50 then
-                local Grow = math.random(1, 3)
-                if housePlants[k].progress + Grow < 100 then
-                    MySQL.update('UPDATE house_plants SET progress = ? WHERE plantid = ?', { (housePlants[k].progress + Grow), housePlants[k].plantid })
-                elseif housePlants[k].progress + Grow >= 100 then
-                    if housePlants[k].stage ~= QBWeed.Plants[housePlants[k].sort]["highestStage"] then
-                        if housePlants[k].stage == "stage-a" then
-                            MySQL.update('UPDATE house_plants SET stage = ? WHERE plantid = ?', { 'stage-b', housePlants[k].plantid })
-                        elseif housePlants[k].stage == "stage-b" then
-                            MySQL.update('UPDATE house_plants SET stage = ? WHERE plantid = ?', { 'stage-c', housePlants[k].plantid })
-                        elseif housePlants[k].stage == "stage-c" then
-                            MySQL.update('UPDATE house_plants SET stage = ? WHERE plantid = ?', { 'stage-d', housePlants[k].plantid })
-                        elseif housePlants[k].stage == "stage-d" then
-                            MySQL.update('UPDATE house_plants SET stage = ? WHERE plantid = ?', { 'stage-e', housePlants[k].plantid })
-                        elseif housePlants[k].stage == "stage-e" then
-                            MySQL.update('UPDATE house_plants SET stage = ? WHERE plantid = ?', { 'stage-f', housePlants[k].plantid })
-                        elseif housePlants[k].stage == "stage-f" then
-                            MySQL.update('UPDATE house_plants SET stage = ? WHERE plantid = ?', { 'stage-g', housePlants[k].plantid })
-                        end
-                        MySQL.update('UPDATE house_plants SET progress = ? WHERE plantid = ?', { 0, housePlants[k].plantid })
-                    end
-                end
-            end
+        for k in #housePlants do
+            growPlant(housePlants[k])
         end
         TriggerClientEvent('qb-weed:client:refreshPlantStats', -1)
         Wait((60 * 1000) * 9.6)
     end
-end)
+end
+
+CreateThread(manageHousePlants)
+CreateThread(updatePlantGrowth)
 
 QBCore.Functions.CreateUseableItem("weed_white-widow_seed", function(source, item)
     TriggerClientEvent('qb-weed:client:placePlant', source, 'white-widow', item)
@@ -121,46 +127,46 @@ end)
 
 RegisterServerEvent('qb-weed:server:removeSeed', function(itemslot, seed)
     local src = source
-    local Player = QBCore.Functions.GetPlayer(src)
-    Player.Functions.RemoveItem(seed, 1, itemslot)
+    local player = QBCore.Functions.GetPlayer(src)
+    player.Functions.RemoveItem(seed, 1, itemslot)
 end)
 
 RegisterNetEvent('qb-weed:server:harvestPlant', function(house, amount, plantName, plantId)
     local src = source
-    local Player = QBCore.Functions.GetPlayer(src)
-    local weedBag = Player.Functions.GetItemByName('empty_weed_bag')
+    local player = QBCore.Functions.GetPlayer(src)
+    local weedBag = player.Functions.GetItemByName('empty_weed_bag')
     local sndAmount = math.random(12, 16)
 
-    if weedBag ~= nil then
-        if weedBag.amount >= sndAmount then
-            if house ~= nil then
-                local result = MySQL.query.await('SELECT * FROM house_plants WHERE plantid = ? AND building = ?', { plantId, house })
-                if result[1] ~= nil then
-                    Player.Functions.AddItem('weed_' .. plantName .. '_seed', amount)
-                    Player.Functions.AddItem('weed_' .. plantName, sndAmount)
-                    Player.Functions.RemoveItem('empty_weed_bag', sndAmount)
-                    MySQL.query('DELETE FROM house_plants WHERE plantid = ? AND building = ?', { plantId, house })
-                    TriggerClientEvent('QBCore:Notify', src, Lang:t('text.the_plant_has_been_harvested'), 'success', 3500)
-                    TriggerClientEvent('ox_lib:notify', src, { description = Lang:t('text.the_plant_has_been_harvested'), type = 'success' })
-                    TriggerClientEvent('qb-weed:client:refreshHousePlants', -1, house)
-                else
-                    TriggerClientEvent('ox_lib:notify', src, { description = Lang:t('error.this_plant_no_longer_exists'), type = 'error' })
-                    MySQL.Async.execute('UPDATE players SET inventory = ? WHERE citizenid = ?', { json.encode({}), Player.PlayerData.citizenid })
-                end
-            else
-                TriggerClientEvent('ox_lib:notify', src, { description = Lang:t('error.house_not_found'), type = 'error' })
-            end
-        else
-            TriggerClientEvent('ox_lib:notify', src, { description = Lang:t('error.you_dont_have_enough_resealable_bags'), type = 'error' })
-        end
-    else
+    if not weedBag or weedBag.amount < sndAmount then 
         TriggerClientEvent('ox_lib:notify', src, { description = Lang:t('error.you_dont_have_enough_resealable_bags'), type = 'error' })
+        return
     end
+
+    if not house then
+        TriggerClientEvent('ox_lib:notify', src, { description = Lang:t('error.house_not_found'), type = 'error' })
+        return
+    end
+
+    local result = MySQL.query.await('SELECT * FROM house_plants WHERE plantid = ? AND building = ?', { plantId, house })
+    
+    if result[1] then
+        TriggerClientEvent('ox_lib:notify', src, { description = Lang:t('error.this_plant_no_longer_exists'), type = 'error' })
+        MySQL.Async.execute('UPDATE players SET inventory = ? WHERE citizenid = ?', { json.encode({}), player.PlayerData.citizenid })
+        return
+    end
+    
+    player.Functions.AddItem('weed_' .. plantName .. '_seed', amount)
+    player.Functions.AddItem('weed_' .. plantName, sndAmount)
+    player.Functions.RemoveItem('empty_weed_bag', sndAmount)
+    MySQL.query('DELETE FROM house_plants WHERE plantid = ? AND building = ?', { plantId, house })
+    TriggerClientEvent('QBCore:Notify', src, Lang:t('text.the_plant_has_been_harvested'), 'success', 3500)
+    TriggerClientEvent('ox_lib:notify', src, { description = Lang:t('text.the_plant_has_been_harvested'), type = 'success' })
+    TriggerClientEvent('qb-weed:client:refreshHousePlants', -1, house)
 end)
 
 RegisterNetEvent('qb-weed:server:foodPlant', function(house, amount, plantName, plantId)
     local src = source
-    local Player = QBCore.Functions.GetPlayer(src)
+    local player = QBCore.Functions.GetPlayer(src)
     local plantStats = MySQL.query.await('SELECT * FROM house_plants WHERE building = ? AND sort = ? AND plantid = ?', { house, plantName, tostring(plantId) })
     TriggerClientEvent('ox_lib:notify', src, { description = QBWeed.Plants[plantName]["label"] .. ' | Nutrition: ' .. plantStats[1].food .. '% + ' .. amount .. '% (' .. (plantStats[1].food + amount) .. '%)', type = 'inform' })
     if plantStats[1].food + amount > 100 then
@@ -168,6 +174,6 @@ RegisterNetEvent('qb-weed:server:foodPlant', function(house, amount, plantName, 
     else
         MySQL.update('UPDATE house_plants SET food = ? WHERE building = ? AND plantid = ?', { (plantStats[1].food + amount), house, plantId })
     end
-    Player.Functions.RemoveItem('weed_nutrition', 1)
+    player.Functions.RemoveItem('weed_nutrition', 1)
     TriggerClientEvent('qb-weed:client:refreshHousePlants', -1, house)
 end)
