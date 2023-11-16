aad2b62b68cebca3bd64487e823e8bbd1fd2ae87
Matthew <22198949+MafewTM@users.noreply.github.com>
2023-11-16 13:47:40 -0500

    fix: closing

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -6,7 +6,7 @@ repository 'https://github.com/Qbox-project/qbx_weed'
 version '1.0.0'
 
 shared_scripts {
-    '@ox_lib/init.lua'
+    '@ox_lib/init.lua',
     '@qbx_core/modules/utils.lua',
     '@qbx_core/shared/locale.lua',
     'locales/en.lua',
