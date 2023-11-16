aa7310d2163be06e4f44d014ada5661371498ebc
Matthew <22198949+MafewTM@users.noreply.github.com>
2023-11-16 14:00:38 -0500

    fix: import modules
    * fix: import modules
    
    * fix: closing

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,16 +1,17 @@
 fx_version 'cerulean'
 game 'gta5'
 
-description 'QBX-Weed'
-repository 'https://github.com/Qbox-project/qbx-weed'
+description 'QBX_Weed'
+repository 'https://github.com/Qbox-project/qbx_weed'
 version '1.0.0'
 
 shared_scripts {
-    '@qbx_core/import.lua',
-    'config.lua',
+    '@ox_lib/init.lua',
+    '@qbx_core/modules/utils.lua',
     '@qbx_core/shared/locale.lua',
-    'locales/en.lua', -- Change to the language you want
-    '@ox_lib/init.lua'
+    'locales/en.lua',
+    'locales/*.lua',
+    'config.lua',
 }
 
 client_script 'client/main.lua'
@@ -19,8 +20,6 @@ server_scripts {
     '@oxmysql/lib/MySQL.lua',
     'server/main.lua'
 }
-
-modules {'qbx_core:utils'}
 
 provide 'qb-weed'
 lua54 'yes'
