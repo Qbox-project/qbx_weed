6876f0e8bd913a2bf576e3f1cfd6733d48f1b629
BerkieBb <82737367+BerkieBb@users.noreply.github.com>
2024-09-07 14:49:51 +0200

    refactor(client)!: rework for outside placement
    - Added outside placement
    - Corrected for qbx_properties
    - Added types
    - Fixed bug of objects spawning on top of each other

--- client/main.lua
+++ client/main.lua
@@ -1,65 +1,121 @@
+---@type WeedSharedConfig
 local sharedConfig = require 'config.shared'
-local housePlants = {}
-local insideHouse = false
-local currentHouse = nil
+---@type WeedClientConfig
+local config = require 'config.client'
+---@type table<string, WeedPlant[]>
+local propertyPlants = {}
+---@type WeedPlant[]
+local outsidePlants = {}
+local currentProperty = exports.qbx_core:GetPlayerData()?.metadata.currentPropertyId
 local plantsSpawned = false
 local closestTarget = 0
 
-local function spawnPlants()
-    if plantsSpawned then return end
-    for _, v in pairs(housePlants[currentHouse]) do
-        local plantProp = CreateObject(joaat(sharedConfig.plants[v.sort].stages[v.stage]), v.coords.x, v.coords.y, v.coords.z, false, false, false)
-        while not plantProp do Wait(0) end
+currentProperty = currentProperty and tostring(currentProperty) or nil
+
+local function spawnPropertyPlants()
+    if plantsSpawned or not currentProperty then return end
+
+    for i = 1, #propertyPlants[currentProperty] do
+        local plant = propertyPlants[currentProperty][i]
+        local plantProp = CreateObject(joaat(sharedConfig.plants[plant.sort].stages[plant.stage]), plant.coords.x, plant.coords.y, plant.coords.z, false, false, false)
+        while not plantProp do
+            Wait(0)
+        end
 
         PlaceObjectOnGroundProperly(plantProp)
         Wait(10)
         FreezeEntityPosition(plantProp, true)
         SetEntityAsMissionEntity(plantProp, false, false)
     end
+
     plantsSpawned = true
 end
 
-local function deleteClosestPlant(plantStage, plantData)
-    if not (plantStage and plantData) then return end
+local function spawnOutsidePlants()
+    if plantsSpawned or currentProperty then return end
 
-    local closestPlant = GetClosestObjectOfType(plantData.coords.x, plantData.coords.y, plantData.coords.z, 3.5, joaat(plantStage), false, false, false)
+    for i = 1, #outsidePlants do
+        local plant = outsidePlants[i]
+        local plantProp = CreateObject(joaat(sharedConfig.plants[plant.sort].stages[plant.stage]), plant.coords.x, plant.coords.y, plant.coords.z, false, false, false)
+        while not plantProp do
+            Wait(0)
+        end
+
+        PlaceObjectOnGroundProperly(plantProp)
+        Wait(10)
+        FreezeEntityPosition(plantProp, true)
+        SetEntityAsMissionEntity(plantProp, false, false)
+    end
+
+    plantsSpawned = true
+end
+
+---@param plantStage 1 | 2 | 3 | 4 | 5 | 6 | 7
+---@param plantData WeedPlant
+local function deleteClosestPlant(plantStage, plantData)
+    if not plantStage or not plantData then return end
+
+    local closestPlant = GetClosestObjectOfType(plantData.coords.x, plantData.coords.y, plantData.coords.z, 3.5, joaat(sharedConfig.plants[plantData.sort].stages[plantStage]), false, false, false)
     if closestPlant == 0 then return end
 
     DeleteObject(closestPlant)
 end
 
-local function despawnPlants()
-    if not (plantsSpawned or currentHouse) then return end
+---@param property string
+local function despawnPropertyPlants(property)
+    if not plantsSpawned or not property then return end
 
-    local plants = housePlants[currentHouse]
+    local plants = propertyPlants[property]
+    if not plants then return end
 
-    for i = #plants, 1, -1 do
+    for i = 1, #plants do
         local plantData = plants[i]
-        for _, stage in pairs(sharedConfig.plants[plantData.sort].stages) do
+        for stage = 1, #sharedConfig.plants[plantData.sort].stages do
             deleteClosestPlant(stage, plantData)
         end
-        table.remove(plants, i)
+
+        plants[i] = nil
+    end
+
+    plantsSpawned = false
+end
+
+---@param property string
+local function despawnOutsidePlants(property)
+    if not plantsSpawned or property then return end
+
+    for i = 1, #outsidePlants do
+        local plantData = outsidePlants[i]
+        for stage = 1, #sharedConfig.plants[plantData.sort].stages do
+            deleteClosestPlant(stage, plantData)
+        end
+
+        outsidePlants[i] = nil
     end
 
     plantsSpawned = false
 end
 
 local function updatePlantStats()
-    if not (insideHouse or plantsSpawned) then return end
-    for k, v in pairs(housePlants[currentHouse]) do
-        local gender = v.gender == 'female' and 'F' or 'M'
-        local plyDistance = #(GetEntityCoords(cache.ped) - v.coords)
+    if not plantsSpawned then return 1000 end
 
+    local tbl = currentProperty and propertyPlants[currentProperty] or outsidePlants
+    local sleep = 1000
+    for i = 1, #tbl do
+        local plant = tbl[i]
+        local gender = plant.gender == 'female' and 'F' or 'M'
+        local plyDistance = #(GetEntityCoords(cache.ped) - plant.coords)
         if plyDistance < 0.8 then
-            closestTarget = k
-            if v.health > 0 then
-                if v.stage ~= sharedConfig.plants[v.sort].highestStage then
-                    local label = ('%s%s~w~ [%s] | %s ~b~%s~w~ | %s ~b~%s~w~'):format(locale('text.sort'), sharedConfig.plants[v.sort].label, gender, locale('text.nutrition'), v.food, locale('text.health'), v.health)
-                    qbx.drawText3d({text = label, coords = v.coords})
+            closestTarget = i
+            if plant.health > 0 then
+                sleep = 0
+                if plant.stage ~= #sharedConfig.plants[plant.sort].stages then
+                    local label = ('%s%s~w~ [%s] | %s ~b~%s~w~ | %s ~b~%s~w~'):format(locale('text.sort'), sharedConfig.plants[plant.sort].label, gender, locale('text.nutrition'), plant.food, locale('text.health'), plant.health)
+                    qbx.drawText3d({text = label, coords = plant.coords})
                 else
-                    local label = ('%s ~g~%s~w~ [%s] | %s ~b~%s~w~ | %s ~b~%s~w~'):format(locale('text.sort'), sharedConfig.plants[v.sort].label, gender, locale('text.nutrition'), v.food, locale('text.health'), v.health)
-                    qbx.drawText3d({text = locale('text.harvest_plant'), coords = vec3(v.coords.x, v.coords.y, v.coords.z + 0.2)})
-                    qbx.drawText3d({text = label, coords = v.coords})
+                    local label = ('%s ~g~%s~w~ [%s] | %s ~b~%s~w~ | %s ~b~%s~w~'):format(locale('text.sort'), sharedConfig.plants[plant.sort].label, gender, locale('text.nutrition'), plant.food, locale('text.health'), plant.health)
+                    qbx.drawText3d({text = locale('text.harvest_plant'), coords = vec3(plant.coords.x, plant.coords.y, plant.coords.z + 0.2)})
+                    qbx.drawText3d({text = label, coords = plant.coords})
                     if IsControlJustPressed(0, 38) then
                         if lib.progressCircle({
                             duration = 8000,
@@ -79,20 +135,18 @@ local function updatePlantStats()
                             },
                         })
                         then
-                            ClearPedTasks(cache.ped)
-                            local amount = math.random(1, 6)
-                            if gender == 'M' then
-                                amount = math.random(1, 2)
-                            end
-                            TriggerServerEvent('qbx_weed:server:harvestPlant', currentHouse, amount, sharedConfig.plants[v.sort].item, v.id, v.coords)
+                            local amount = gender == 'M' and math.random(1, 2) or math.random(1, 6)
+                            TriggerServerEvent('qbx_weed:server:harvestPlant', currentProperty, amount, sharedConfig.plants[plant.sort].item, plant.id, plant.coords)
                         else
-                            ClearPedTasks(cache.ped)
                             exports.qbx_core:Notify(locale('error.process_canceled'), 'error')
                         end
+
+                        ClearPedTasks(cache.ped)
                     end
                 end
-            elseif v.health == 0 then
-                qbx.drawText3d({text = locale('error.plant_has_died'), coords = v.coords})
+            elseif plant.health == 0 then
+                sleep = 0
+                qbx.drawText3d({text = locale('error.plant_has_died'), coords = plant.coords})
                 if IsControlJustPressed(0, 38) then
                     if lib.progressCircle({
                             duration = 8000,
@@ -112,133 +166,74 @@ local function updatePlantStats()
                             },
                         })
                     then
-                        ClearPedTasks(cache.ped)
-                        TriggerServerEvent('qbx_weed:server:removeDeathPlant', currentHouse, v.id, v.coords)
+                        TriggerServerEvent('qbx_weed:server:removeDeadPlant', currentProperty, plant.id, plant.coords)
                     else
-                        ClearPedTasks(cache.ped)
                         exports.qbx_core:Notify(locale('error.process_canceled'), 'error')
                     end
+
+                    ClearPedTasks(cache.ped)
                 end
             end
         end
     end
+
+    return sleep
 end
 
-local function updatePlants()
-    local sleep = 0
-    while true do
-        Wait(sleep)
-        sleep = 0
-        updatePlantStats()
-        if not insideHouse then
-            sleep = 5000
-        end
-    end
-end
-
-CreateThread(updatePlants)
-
-RegisterNetEvent('qb-weed:client:getHousePlants', function(house)
-    local plants = lib.callback.await('qbx_weed:server:getBuildingPlants', false, house)
-    currentHouse = house
-    housePlants[currentHouse] = plants
-    insideHouse = true
-    spawnPlants()
-end)
-
-RegisterNetEvent('qb-weed:client:leaveHouse', function()
-    despawnPlants()
-    Wait(1000)
-    if not currentHouse then return end
-    insideHouse = false
-    housePlants[currentHouse] = nil
-    currentHouse = nil
-end)
-
-RegisterNetEvent('qbx_weed:client:refreshHousePlants', function(house)
-    if not currentHouse or currentHouse ~= house then return end
-    despawnPlants()
-    Wait(1000)
-    local plants = lib.callback.await('qbx_weed:server:getBuildingPlants', false, house)
-    currentHouse = house
-    housePlants[currentHouse] = plants
-    spawnPlants()
-end)
-
-RegisterNetEvent('qbx_weed:client:refreshPlantStats', function()
-    if not insideHouse or not currentHouse then return end
-    despawnPlants()
-    Wait(1000)
-    local plants = lib.callback.await('qbx_weed:server:getBuildingPlants', false, currentHouse)
-    housePlants[currentHouse] = plants
-    spawnPlants()
-end)
-
-RegisterNetEvent('qbx_weed:client:placePlant', function(type, item)
+exports('placePlant', function(type, item)
     local plyCoords = GetOffsetFromEntityInWorldCoords(cache.ped, 0, 0.75, 0)
     local closestPlant = 0
-    for _, v in pairs(sharedConfig.props) do
+    for i = 1, #sharedConfig.stageProps do
         if closestPlant == 0 then
-            closestPlant = GetClosestObjectOfType(plyCoords.x, plyCoords.y, plyCoords.z, 0.8, joaat(v), false, false, false)
+            closestPlant = GetClosestObjectOfType(plyCoords.x, plyCoords.y, plyCoords.z, 0.8, joaat(sharedConfig.stageProps[i]), false, false, false)
         end
     end
 
-    if currentHouse then
-        if closestPlant == 0 then
-            LocalPlayer.state:set('invBusy', true, true)
-            if lib.progressCircle({
-                duration = 8000,
-                position = 'bottom',
-                label = locale('text.planting'),
-                useWhileDead = false,
-                canCancel = true,
-                disable = {
-                    move = true,
-                    car = true,
-                    mouse = false,
-                    combat = true,
-                },
-                anim = {
-                    dict = 'amb@world_human_gardener_plant@male@base',
-                    clip = 'base',
-                },
-            })
-            then
-                ClearPedTasks(cache.ped)
-                TriggerServerEvent('qbx_weed:server:placePlant', plyCoords, type, currentHouse)
-                TriggerServerEvent('qbx_weed:server:removeSeed', item.slot, item.name)
-            else
-                ClearPedTasks(cache.ped)
-                exports.qbx_core:Notify(locale('error.process_canceled'), 'error')
-                LocalPlayer.state:set('invBusy', false, true)
-            end
-        else
-            exports.qbx_core:Notify(locale('error.cant_place_here'), 'error')
-        end
-    else
-        exports.qbx_core:Notify(locale('error.not_safe_here'), 'error')
+    if closestPlant ~= 0 then
+        return exports.qbx_core:Notify(locale('error.cant_place_here'), 'error')
     end
+
+    LocalPlayer.state:set('invBusy', true, true)
+    if lib.progressCircle({
+        duration = 8000,
+        position = 'bottom',
+        label = locale('text.planting'),
+        useWhileDead = false,
+        canCancel = true,
+        disable = {
+            move = true,
+            car = true,
+            mouse = false,
+            combat = true,
+        },
+        anim = {
+            dict = 'amb@world_human_gardener_plant@male@base',
+            clip = 'base',
+        },
+    })
+    then
+        TriggerServerEvent('qbx_weed:server:placePlant', plyCoords, type, currentProperty)
+        TriggerServerEvent('qbx_weed:server:removeSeed', item.slot, item.name)
+    else
+        exports.qbx_core:Notify(locale('error.process_canceled'), 'error')
+    end
+
+    ClearPedTasks(cache.ped)
 end)
 
-RegisterNetEvent('qbx_weed:client:foodPlant', function()
-    if not currentHouse then return end
-
-    if closestTarget ~= 0 then
-        exports.qbx_core:Notify(locale('error.not_safe_here'), 'error')
-        return
+exports('foodPlant', function()
+    if closestTarget == 0 then
+        return exports.qbx_core:Notify(locale('error.not_safe_here'), 'error')
     end
 
-    local data = housePlants[currentHouse][closestTarget]
+    local data = currentProperty and propertyPlants[currentProperty][closestTarget] or outsidePlants[closestTarget]
     local plyDistance = #(GetEntityCoords(cache.ped) - data.coords)
-
     if plyDistance >= 1.0 then
-        exports.qbx_core:Notify(locale('error.cant_place_here'), 'error')
-        return
+        return exports.qbx_core:Notify(locale('error.cant_place_here'), 'error')
     end
 
     if data.food == 100 then
-        exports.qbx_core:Notify(locale('error.not_need_nutrition'), 'error')
-        return
+        return exports.qbx_core:Notify(locale('error.not_need_nutrition'), 'error')
     end
 
     LocalPlayer.state:set('invBusy', true, true)
@@ -260,17 +255,114 @@ RegisterNetEvent('qbx_weed:client:foodPlant', function()
             },
         })
     then
-        ClearPedTasks(cache.ped)
         local newFood = math.random(40, 60)
-        TriggerServerEvent('qbx_weed:server:foodPlant', currentHouse, newFood, data.sort, data.id)
+        TriggerServerEvent('qbx_weed:server:foodPlant', currentProperty, newFood, data.sort, data.id)
     else
-        ClearPedTasks(cache.ped)
-        LocalPlayer.state:set('invBusy', false, true)
         exports.qbx_core:Notify(locale('error.process_canceled'), 'error')
     end
+
+    ClearPedTasks(cache.ped)
+end)
+
+RegisterNetEvent('qbx_core:client:onSetMetaData', function(meta, oldValue, value)
+    if meta ~= 'currentPropertyId' then return end
+
+    currentProperty = value and tostring(value) or nil
+
+    if currentProperty then
+        despawnOutsidePlants(oldValue)
+
+        Wait(1000)
+
+        outsidePlants = {}
+
+        local plants = lib.callback.await('qbx_weed:server:getPropertyPlants', false, currentProperty)
+        propertyPlants[currentProperty] = plants
+        spawnPropertyPlants()
+    else
+        despawnPropertyPlants(oldValue)
+
+        Wait(1000)
+
+        propertyPlants[oldValue] = nil
+
+        Wait(2000) -- Wait for server to send outsidePlants
+
+        spawnOutsidePlants()
+    end
+end)
+
+RegisterNetEvent('qbx_weed:client:refreshPropertyPlants', function(property)
+    if not currentProperty or currentProperty ~= property then return end
+
+    despawnPropertyPlants(currentProperty)
+    despawnOutsidePlants(currentProperty)
+
+    Wait(1000)
+
+    local plants = lib.callback.await('qbx_weed:server:getPropertyPlants', false, property)
+    propertyPlants[currentProperty] = plants
+    spawnPropertyPlants()
+end)
+
+RegisterNetEvent('qbx_weed:client:refreshPlantStats', function()
+    despawnPropertyPlants(currentProperty)
+    despawnOutsidePlants(currentProperty)
+
+    Wait(1000)
+
+    if currentProperty then
+        local plants = lib.callback.await('qbx_weed:server:getPropertyPlants', false, currentProperty)
+        propertyPlants[currentProperty] = plants
+        spawnPropertyPlants()
+    else
+        spawnOutsidePlants()
+    end
+end)
+
+---@param plants table<number, vector3>
+RegisterNetEvent('qbx_weed:client:refreshOutsidePlants', function(plants)
+    if source == '' or GetInvokingResource() then return end
+
+    if currentProperty then
+        if table.type(outsidePlants) ~= 'empty' then
+            despawnOutsidePlants(currentProperty)
+
+            Wait(1000)
+
+            outsidePlants = {}
+        end
+
+        return
+    end
+
+    despawnOutsidePlants(currentProperty)
+
+    Wait(1000)
+
+    outsidePlants = {}
+
+    local closePlants = {}
+    local playerCoords = GetEntityCoords(cache.ped)
+    for k, v in pairs(plants) do
+        if #(playerCoords - v) < config.outsidePlantsDistance then
+            closePlants[#closePlants + 1] = k
+        end
+    end
+
+    outsidePlants = lib.callback.await('qbx_weed:server:getOutsidePlants', false, closePlants)
+    spawnOutsidePlants()
 end)
 
 AddEventHandler('onResourceStop', function(resource)
     if resource ~= GetCurrentResourceName() then return end
-    despawnPlants()
+
+    despawnPropertyPlants(currentProperty)
+    despawnOutsidePlants(currentProperty)
+end)
+
+CreateThread(function()
+    while true do
+        Wait(updatePlantStats())
+    end
 end)
